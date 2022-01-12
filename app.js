9ee1260a4a99ee83f3ae65c738f50bfa7985d9a0
Alabhya Jindal <alabhya10@gmail.com>
2022-01-12 21:19:08 +0530

    functional cross

--- app.js
+++ app.js
@@ -1,26 +1,23 @@
 function addMore() {
   let newTask = document.getElementById("text-entry").value;
+  if (newTask == "") {
+  } else {
+    let ul = document.getElementById("todo-list");
+    let li = document.createElement("li");
+    li.textContent = newTask;
 
-  let ul = document.getElementById("todo-list");
-  let li = document.createElement("li");
-  li.textContent = newTask;
+    li.insertAdjacentHTML(
+      "beforeend",
+      '<ion-icon name="close-outline"></ion-icon>'
+    );
+    ul.appendChild(li);
 
-  li.insertAdjacentHTML(
-    "beforeend",
-    '<ion-icon name="close-outline"></ion-icon>'
-  );
-  ul.appendChild(li);
-
-  document.getElementById("text-entry").value = "";
+    document.getElementById("text-entry").value = "";
+  }
 }
 
 let ul = document.querySelector("ul");
-
-// Aim for zero erros - the below will not work
-
-ul.addEventListener("click", () => {
-  let x = document.querySelector("ion-icon");
-  x.addEventListener("click", () => {
-    x.parentNode.remove();
-  });
+ul.addEventListener("click", (e) => {
+  let li = e.target.parentNode;
+  ul.removeChild(li);
 });
